4a15106347ab28e626c9712129d8ddb5a831db6d
Jinwen <zhang1995jinwen@gmail.com>
2019-11-18 08:54:21 -0500

    first checkin: main page and basics of network request setup

--- app/src/main/java/com/example/hearthstonehub/CardManager.kt
+++ app/src/main/java/com/example/hearthstonehub/CardManager.kt
@@ -7,4 +7,20 @@ import org.json.JSONObject
 
 
 class CardManager {
+
+
+    private val okHttpClient: OkHttpClient
+
+    init {
+
+        val builder = OkHttpClient.Builder()
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+
+        builder.addInterceptor(logging)
+        okHttpClient = builder.build()
+    }
+
+
+
 }
